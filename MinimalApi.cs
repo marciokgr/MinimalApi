b7892c2e07760191ab9b79b5c9509990850efcb9
Márcio Krüger <marcio.kruger@ailos.coop.br>
2021-11-17 17:58:06 -0300

    Organizando Rotas

---
+++ MinimalApi.cs
@@ -0,0 +1,35 @@
+﻿using MinimalApi.ViewModels;
+using MinimalApi.Data;
+
+namespace MinimalApi
+{
+    public static class MinimalApi
+    {
+        public static void MapMinimalRoutes(this IEndpointRouteBuilder app)
+        {
+            //Verbos
+            app.MapGet("v1/todos", (AppDbContext context) =>
+            {
+                var todos = context.Todos;
+                return todos is not null ? Results.Ok(todos) : Results.NotFound();
+            });
+
+            //Recebendo Post
+            app.MapPost("/v1/todos", (AppDbContext context, CreateTodoViewModel model) =>
+            {
+                var todo = model.MapTo();
+                //Valida o objeto
+                if (!model.IsValid)
+                    return Results.BadRequest(model.Notifications);
+
+                //Criando objeto no banco de dados
+                context.Todos.Add(todo);
+
+                //Salva no banco de dados.
+                context.SaveChanges();
+
+                return Results.Created($"/v1/todos/{todo.Id}", todo);
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using MinimalApi.Data;
-using MinimalApi.ViewModels;
-//using MinimalApi.Models;
+using MinimalApi;
 
 //Criando projeto WEB
 var builder = WebApplication.CreateBuilder(args);
@@ -14,31 +13,7 @@ builder.Services.AddSwaggerGen();
 
 //Instancia do APP
 var app = builder.Build();
-
-
-//Verbos
-app.MapGet("v1/todos", (AppDbContext context) =>
-{
-    var todos = context.Todos;
-    return todos is not null ? Results.Ok(todos) : Results.NotFound();
-});
-
-//Recebendo Post
-app.MapPost("/v1/todos", (AppDbContext context, CreateTodoViewModel model) =>
-{
-    var todo = model.MapTo();
-    //Valida o objeto
-    if (!model.IsValid)
-        return Results.BadRequest(model.Notifications);
-
-    //Criando objeto no banco de dados
-    context.Todos.Add(todo);
-    
-    //Salva no banco de dados.
-    context.SaveChanges();
-
-    return Results.Created($"/v1/todos/{todo.Id}", todo);
-});
+app.MapMinimalRoutes();
 
 //Swagger
 app.UseSwagger();
